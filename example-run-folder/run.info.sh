b897a90e7cfb1848f62789b69a06629351cc9607
ZLZLab <zhengzongli@gmail.com>
2018-04-26 01:08:43 +0800

    add example datasets

---
+++ example-run-folder/run.info.sh
@@ -0,0 +1,17 @@
+# modify below line
+SplitFusionPATH=path-to-the-SplitFusion
+analysisRunFull=panoseq/analysis/example-run-folder
+DEPATH=/data/db
+REPPATH=/tools/repo
+anaID=example-run-folder
+
+
+# default parameters for calling breakpoints
+samplesheetName=example
+StrVarMinUniqMap=3
+minBreakDistance=1000
+maxQueryGap=1
+minMapLength=25
+minExclusive=25
+maxOverlap=9
+minMQ=20
